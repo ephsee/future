8c5a9fba6d5cfbdd1a1af22dbdce9ec63530ecf4
Francois Spies <fcspies@gmail.com>
2022-05-13 13:21:35 -0400

    styled components

--- future-client/src/App.js
+++ future-client/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const Heading1 = styled.h1`
+    font-family: monospace;
+    text-align: center;
+    color: red;
+`
+const Container1 = styled.div`
+    background-color: black;
+    border-radius: 15px;
+`
+
+const Container2 = styled.div`
+    color: papayawhip;
+    text-align: center;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+`
+
+const Heading2 = styled.h2`
+    font-family: Times New Roman;
+    color: blue;
+    text-align: center;
+`
 
 function App() {
+
+  const set = [1,2,3,4,5,6,7,8,9,10]
+
+  const showSet = set.map( n => <Container2>{n}</Container2>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container1>
+          <Heading1> BANANA </Heading1>
+          <Heading2> MORE BANANA</Heading2>
+
+          {showSet}
+
+    </Container1>
   );
 }
 
